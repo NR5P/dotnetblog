4b65795c75cd4d0b6985ef356b2f9474db88d537
NR5P <orangev8zcar@gmail.com>
2023-01-15 19:22:45 -0600

    added meta data for blog posts

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -8,6 +8,8 @@ namespace dotnethomepage.Models
         public string title {get; set;}
         public string shortBody {get; set;}
         public string body {get; set;}
+        public string metaDescription {get; set;}
+        public string metaImage {get; set;}
         public DateTime postDate {get; set;}
     }
 }
